bb92d09d0068c52027c75bbd14bdf3c957b49127
Nonn34 <weeraphong.jrwn@gmail.com>
2023-09-29 14:32:21 +0700

    add

---
+++ src/components/Greeting.tsx
@@ -0,0 +1,21 @@
+import classes from './Greeting.module.css'
+
+const Greeting = () => {
+  return (
+    <div className={classes.test}>
+      <h1>
+        <i>Hello !</i>
+      </h1>
+      <br />
+      <button>
+        <i>
+          <mark>
+            <b> Nonn </b>
+          </mark>
+        </i>
+      </button>
+    </div>
+  )
+}
+
+export default Greeting
